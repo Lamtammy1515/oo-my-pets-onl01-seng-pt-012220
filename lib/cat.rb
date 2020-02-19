133a76f12a523d08d6749f7b0a499b31dd1af63c
Tammy Lam <lamtammy1515@gmail.com>
2020-02-19 04:06:05 +0000

    Automatically backed up by Learn

--- lib/cat.rb
+++ lib/cat.rb
@@ -1,3 +1,21 @@
 class Cat
-  # code goes here
+  attr_reader :name
+  attr_accessor :owner, :mood
+  @@all = []
+  
+  def initialize(name, owner)
+    @name = name 
+    @owner = owner 
+    @mood = "playful"
+    @@all << self 
+  end 
+    
+  def self.all 
+    @@all 
+  end 
+  
+  def mood(moods)
+    @cat_mood = moods
+  end 
+  
 end
--- lib/owner.rb
+++ lib/owner.rb
@@ -1,3 +1,38 @@
 class Owner
-  # code goes here
+   attr_reader :name, :species
+   @@all = []
+   def initialize(name)
+     @name = name
+     @species = "human" 
+     @@all << self
+   end 
+ 
+   
+   def self.all 
+     @@all 
+   end 
+   
+   def self.count 
+     self.all.count
+   end 
+   
+   def self.reset_all 
+     self.all.clear
+   end 
+   
+   def say_species
+     "I am a #{species}."
+   end 
+   
+   def cats 
+     Cat.all.select {|x| x.owner == self} 
+   end 
+   
+   def dogs 
+     Dog.all.select {|x| x.owner == self} 
+   end 
+   
+
+     
+  
 end
